d222718128dfb64da1a0773a03afffb88c8e608c
Xingyou Song <xingyousong@google.com>
2023-04-28 06:50:50 -0700

    1. Switch database url to local to allow persistent storage on HDD by default 2. VizierClient by default no longer requires external service creation for initialization and environment variables fully control servicer/stub creation. 3. Also change basic_datastore to ram_datastore.
    PiperOrigin-RevId: 527870616

--- vizier/_src/service/sql_datastore_test.py
+++ vizier/_src/service/sql_datastore_test.py
@@ -15,8 +15,10 @@
 from __future__ import annotations
 
 """Tests for sql_datastore."""
+import os
 import sqlalchemy as sqla
 
+from vizier._src.service import constants
 from vizier._src.service import datastore_test_lib
 from vizier._src.service import sql_datastore
 from vizier._src.service.testing import util as test_util
@@ -44,7 +46,7 @@ class SQLDataStoreTest(datastore_test_lib.DataStoreTestCase):
         )
     )
 
-    engine = sqla.create_engine('sqlite:///:memory:', echo=True)
+    engine = sqla.create_engine(constants.SQL_MEMORY_URL, echo=True)
     self.datastore = sql_datastore.SQLDataStore(engine)
     super().setUp()
 
@@ -74,6 +76,21 @@ class SQLDataStoreTest(datastore_test_lib.DataStoreTestCase):
     self.assertUpdateMetadataAPI(
         self.datastore, self.example_study, self.example_trials
     )
+
+  def test_local_hdd_persistence(self):
+    db_path = os.path.join(googletest.get_default_test_tmpdir(), 'local.db')
+    sql_url = f'sqlite:///{db_path}'
+
+    engine = sqla.create_engine(sql_url, echo=True)
+    datastore = sql_datastore.SQLDataStore(engine)
+
+    datastore.create_study(self.example_study)
+
+    engine2 = sqla.create_engine(sql_url, echo=True)
+    datastore2 = sql_datastore.SQLDataStore(engine2)
+    study = datastore2.load_study(self.example_study.name)
+
+    self.assertEqual(self.example_study, study)
 
 
 if __name__ == '__main__':
